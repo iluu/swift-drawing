80cdbe5588183e9ab1db372472eacdc883dd9e47
Karolina Kafel <kr@soundcloud.com>
2016-05-13 13:55:49 +0200

    Drawing a simple face

---
+++ Happiness/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Happiness
+//
+//  Created by Karolina Kafel on 13/05/16.
+//  Copyright © 2016 Karolina Kafel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
