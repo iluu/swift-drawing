d1592a10a0166fe8bac763db9b792e8c6637d7c0
Karolina Kafel <kr@soundcloud.com>
2016-07-03 15:54:52 +0200

    Add pan gesture recognizer

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -17,6 +17,24 @@ class HappinessViewController: UIViewController, FaceViewDataSource {
         }
     }
 
+    private struct Constants {
+        static let HappinsessGestureScale: CGFloat = 4
+    }
+
+    @IBAction func changeHappiness(sender: UIPanGestureRecognizer) {
+        switch (sender.state) {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = sender.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinsessGestureScale)
+            if happiness != 0 {
+                happiness += happinessChange
+                sender.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+
     var happiness: Int = 75 {
         // 0 = very sad, 100 = ecstatic
         didSet {
